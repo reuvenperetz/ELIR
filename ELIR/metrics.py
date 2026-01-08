ae481abef839213087e6c69c7e4f9f1f4668d857
Reuven Peretz <reuven.peretz@arm.com>
2026-01-08 19:00:53 +0200

    support fid

--- ELIR/metrics.py
+++ ELIR/metrics.py
@@ -11,24 +11,27 @@ class MetricEval(object):
         self.metric = metric
         self.values = {}  # Dict: dataloader_idx -> list of values
         self.count = {}   # Dict: dataloader_idx -> count
-        self.image_size = 512
+        self.image_size = {}  # Dict: dataloader_idx -> image_size
         self.out_dir = out_dir
         self.device = device
+
+        # Per-dataloader directories for metrics that save images
+        self.dl_out_dirs = {}  # Dict: dataloader_idx -> output_dir
+        self.pred_dirs = {}    # Dict: dataloader_idx -> pred_dir (for fid)
+        self.gt_dirs = {}      # Dict: dataloader_idx -> gt_dir (for fid)
+
         if metric == "fid-g":
             self.evaluater = FrechetInceptionDistance(normalize=True, reset_real_features=False)
             load_fid_statistics(os.path.join(os.path.dirname(__file__), "datasets", "celeba_fid_stat.pt"), self.evaluater, device)
             self.evaluater.to(device)
-        elif metric == "fid-f":
-            self.out_dir = "out" if out_dir is None else os.path.join(self.out_dir, "out")
-            shutil.rmtree(self.out_dir, ignore_errors=True)
-            os.makedirs(self.out_dir, exist_ok=True)
+        elif metric in ["fid", "fid-f"]:
+            # Directories will be created per dataloader_idx in _ensure_dl_idx
             self.evaluater = pyiqa.create_metric('fid', device=device)
         elif metric == "lpips":
             self.evaluater = pyiqa.create_metric(metric, device=device, net="vgg")
         elif metric == "save":
-            self.out_dir = "out" if out_dir is None else os.path.join(self.out_dir, "out")
-            shutil.rmtree(self.out_dir, ignore_errors=True)
-            os.makedirs(self.out_dir, exist_ok=True)
+            # Directories will be created per dataloader_idx in _ensure_dl_idx
+            pass
         else:
             self.evaluater = pyiqa.create_metric(metric, device=device)
 
@@ -36,6 +39,29 @@ class MetricEval(object):
         if dataloader_idx not in self.values:
             self.values[dataloader_idx] = []
             self.count[dataloader_idx] = 0
+            self.image_size[dataloader_idx] = 512
+
+            # Create per-dataloader directories for metrics that save images
+            if self.metric in ["fid", "fid-f", "save"]:
+                base_dir = "out" if self.out_dir is None else self.out_dir
+                if self.out_dir is not None:
+                    os.makedirs(self.out_dir, exist_ok=True)
+
+                dl_out_dir = os.path.join(base_dir, f"{self.metric}_dl{dataloader_idx}")
+                self.dl_out_dirs[dataloader_idx] = dl_out_dir
+
+                shutil.rmtree(dl_out_dir, ignore_errors=True)
+
+                if self.metric == "fid":
+                    # FID needs separate pred and gt directories
+                    pred_dir = os.path.join(dl_out_dir, "pred")
+                    gt_dir = os.path.join(dl_out_dir, "gt")
+                    self.pred_dirs[dataloader_idx] = pred_dir
+                    self.gt_dirs[dataloader_idx] = gt_dir
+                    os.makedirs(pred_dir, exist_ok=True)
+                    os.makedirs(gt_dir, exist_ok=True)
+                else:
+                    os.makedirs(dl_out_dir, exist_ok=True)
 
     def compute(self, x_hq_hat, x_hq=None, dataloader_idx=0):
         self._ensure_dl_idx(dataloader_idx)
@@ -46,18 +72,30 @@ class MetricEval(object):
             self.evaluater.update(x_hq_hat, real=False)
             value = torch.zeros((len(x_hq_hat),))
             self.count[dataloader_idx] += bs
+        elif self.metric == "fid":
+            # FID: save both predictions and ground truth
+            self.image_size[dataloader_idx] = x_hq.shape[-1]
+            pred_dir = self.pred_dirs[dataloader_idx]
+            gt_dir = self.gt_dirs[dataloader_idx]
+            for img, gt in zip(x_hq_hat, x_hq):
+                save_image(img, os.path.join(pred_dir, str(self.count[dataloader_idx]) + ".png"))
+                save_image(gt, os.path.join(gt_dir, str(self.count[dataloader_idx]) + ".png"))
+                self.count[dataloader_idx] += 1
+            value = torch.zeros((len(x_hq_hat),))
         elif self.metric == "fid-f":
-            self.image_size = x_hq.shape[-1]
+            self.image_size[dataloader_idx] = x_hq.shape[-1]
+            dl_out_dir = self.dl_out_dirs[dataloader_idx]
             for img in x_hq_hat:
-                save_image(img, os.path.join(self.out_dir, str(self.count[dataloader_idx]) + ".png"))
+                save_image(img, os.path.join(dl_out_dir, str(self.count[dataloader_idx]) + ".png"))
                 self.count[dataloader_idx] += 1
             value = torch.zeros((len(x_hq_hat),))
         elif self.metric in ["niqe", "clipiqa", "musiq"]:
             value = self.evaluater(x_hq_hat)
             self.count[dataloader_idx] += bs
         elif self.metric == "save":
+            dl_out_dir = self.dl_out_dirs[dataloader_idx]
             for img in x_hq_hat:
-                save_image(img, os.path.join(self.out_dir, str(self.count[dataloader_idx]) + ".png"))
+                save_image(img, os.path.join(dl_out_dir, str(self.count[dataloader_idx]) + ".png"))
                 self.count[dataloader_idx] += 1
             value = torch.zeros((len(x_hq_hat),))
         else:
@@ -71,12 +109,30 @@ class MetricEval(object):
         if self.metric == "fid-g":
             final_value = self.evaluater.compute()
             self.evaluater.reset()
+        elif self.metric == "fid":
+            # FID: compare pred vs gt directories
+            pred_dir = self.pred_dirs[dataloader_idx]
+            gt_dir = self.gt_dirs[dataloader_idx]
+            dl_out_dir = self.dl_out_dirs[dataloader_idx]
+            image_size = self.image_size.get(dataloader_idx, 512)
+
+            final_value = self.evaluater(pred_dir, gt_dir, dataset_res=image_size)
+
+            # Clean up and recreate directories for next use
+            shutil.rmtree(dl_out_dir, ignore_errors=True)
+            os.makedirs(pred_dir, exist_ok=True)
+            os.makedirs(gt_dir, exist_ok=True)
         elif self.metric == "fid-f":
-            final_value = self.evaluater(self.out_dir, dataset_name="FFHQ",
-                                         dataset_res=self.image_size,
+            dl_out_dir = self.dl_out_dirs[dataloader_idx]
+            image_size = self.image_size.get(dataloader_idx, 512)
+
+            final_value = self.evaluater(dl_out_dir, dataset_name="FFHQ",
+                                         dataset_res=image_size,
                                          dataset_split="trainval70k", verbose=False)
-            shutil.rmtree(self.out_dir, ignore_errors=True)
-            os.makedirs(self.out_dir, exist_ok=True)
+
+            # Clean up and recreate directory for next use
+            shutil.rmtree(dl_out_dir, ignore_errors=True)
+            os.makedirs(dl_out_dir, exist_ok=True)
         else:
             final_value = torch.mean(torch.concat(self.values.get(dataloader_idx, [torch.tensor([0.0])])))
 
