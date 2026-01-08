f141fdc140073dadbe60fee0b23ad22b9fd261a0
Reuven Peretz <reuven.peretz@arm.com>
2026-01-08 19:47:33 +0200

    add weight decay option

--- ELIR/training/tparmas.py
+++ ELIR/training/tparmas.py
@@ -3,14 +3,16 @@ import torch
 
 def get_optimizer(train_cfg, model):
     lr = train_cfg.get("lr", 0.0001)
+    weight_decay = train_cfg.get("weight_decay", 0.0)
     optimizer_params = train_cfg.get("optimizer_params", {})
     optimizer_params['lr'] = lr
+    optimizer_params['weight_decay'] = weight_decay
     params = model.parameters()
     optimizer = train_cfg.get("optimizer", None)
     if optimizer:
         return optimizer(params, **optimizer_params)
     else:
-        return torch.optim.adam(params, **optimizer_params)
+        return torch.optim.Adam(params, **optimizer_params)
 
 def get_scheduler(train_cfg, optimizer):
     scheduler_params = train_cfg.get("scheduler_params", {})
